58e3d812941cff5aa963758e17ba0e2476df4e23
xinyu <jolin7180732@gmail.com>
2023-07-10 20:35:31 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect, useReducer } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./Layout";
+import HomePage from "./HomePage";
+import About from "./About";
+import Page404 from "./Page404";
+
+const App = () => {
+  // let [myName, setMyName] = useState("吳小熊");
+
+  // const buttonHandler = () => {
+  //   setMyName("吳大熊");
+  // };
+
+  // // 內部function在畫面渲染時會先執行一次，每當[myName]變更時function又會再執行一次
+  // useEffect(() => {
+  //   console.log("useEffect內部的function正在被執行");
+  // }, [myName]);
+
+  let [messages, setMessages] = useState([]);
+
+  return (
+    // <div>
+    //   <h1>{myName}</h1>
+    //   <button onClick={buttonHandler}>改變姓名</button>
+    // </div>
+
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />}></Route>
+          <Route path="about" element={<About />}></Route>
+          <Route path="*" element={<Page404 />}></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
